02c4ce56d05e4373f72e083ee762aacd4c951b76
Jon Zaro <writetojonzaro@gmail.com>
2020-06-25 15:35:18 -0700

    Forception

---
+++ forception/main.js
@@ -0,0 +1,20 @@
+
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+//CURRENTLY NOT WORKING
+var jonArr = []
+
+function forception(people, alphabet){
+    for(let i = 0; i < people.length; i++){
+        if(people[i] === "Jon"){
+            jonArr.push()
+        }
+    }
+}
+
+console.log(jonArr)
+
+// DESIRED OUTPUT: 
+// ["Jon:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jacob:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jingle:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Heimer:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Schmidt:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
