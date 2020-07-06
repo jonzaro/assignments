acf7615515ad35cc145c7cce42a272ce32ae3aaf
Jon Zaro <writetojonzaro@gmail.com>
2020-07-06 13:03:31 -0700

    Bugs on a Plane

--- employee_records/main.js
+++ employee_records/main.js
@@ -1,15 +1,15 @@
 let employees = []
 
-function employee(name, title, salary, status){
+function Employee(name, title, salary, status){
     this.name = name;
     this.jobTitle = title;
     this.salary = salary;
     this.status = status;
 }
 
-const employee1 = new employee("Jeff", "Instructor", 75000, "Full Time")
-const employee2 = new employee("Daniel", "Teachers Assistant", 50000, "Part Time")    
-const employee3 = new employee("Ashley", "Accountant", 120000, "Full Time")
+const employee1 = new Employee("Jeff", "Instructor", 75000, "Full Time")
+const employee2 = new Employee("Daniel", "Teachers Assistant", 50000, "Part Time")    
+const employee3 = new Employee("Ashley", "Accountant", 120000, "Full Time")
 
 
 function printEmployeeForm(){
