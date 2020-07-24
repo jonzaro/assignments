ed4677740097cdea1587552b3748e21d60ffb2b3
Jon Zaro <writetojonzaro@gmail.com>
2020-07-24 12:23:07 -0700

    Array Methods Overview

--- array_sort/main.js
+++ array_sort/main.js
@@ -48,21 +48,21 @@
 // words.sort();
 // console.log(words);
 
-//5) Sort the objects in the array by age
-// const people = [
-//     { name: "Quiet Samurai", age: 22 },
-//     { name: "Arrogant Ambassador", age: 100 },
-//     { name: "Misunderstood Observer", age: 2 },
-//     { name: "Unlucky Swami", age: 77 }
-// ]
+5) Sort the objects in the array by age
+const people = [
+    { name: "Quiet Samurai", age: 22 },
+    { name: "Arrogant Ambassador", age: 100 },
+    { name: "Misunderstood Observer", age: 2 },
+    { name: "Unlucky Swami", age: 77 }
+]
 
-// function byAge(arr){
-//     arr.sort(function (a, b){
-//         return a.age - b.age;
-//     })
-// }
-// byAge(people)
-// console.log(people);
+function byAge(arr){
+    arr.sort(function (a, b){
+        return a.age - b.age;
+    })
+}
+byAge(people)
+console.log(people);
 
 
 
