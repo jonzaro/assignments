25a94dcd94a6e89264880f71307b029d360e8217
Jon Zaro <writetojonzaro@gmail.com>
2020-12-07 15:37:49 -0800

    axios todo

--- axios_todo/axios-practice/main.js
+++ axios_todo/axios-practice/main.js
@@ -12,6 +12,12 @@ function renderTodo(data){
     const h1 = document.createElement('h1')
     h1.textContent = data.title
     document.body.appendChild(h1)
+    const h3 = document.createElement('h3')
+    h3.textContent = "Destination - " + data.description
+    document.body.appendChild(h3)
+    const h32 = document.createElement('h3')
+    h32.textContent = "Price - $" + data.price
+    document.body.appendChild(h32)
     return data
 //add delete button (event listener?)
 }
@@ -25,14 +31,15 @@ function compile(){
     
 }
 
-// make form for new todo in html
-
 function createTodo(title = "", description = "", price = "", imgUrl =""){
-    return axios.post("https://api.vschool.io/jonzaro/todo", {title: title, description: description, title: title, price: price, imgUrl: imgUrl})
+    return axios.post("https://api.vschool.io/jonzaro/todo", {title: title, description: description, price: price, imgUrl: imgUrl})
 }
 
 function deleteTodo(){
-
+    for (let)
+    //how can i generate a button to delete a todo that was just added.  wouldn't my delete event listener need to
+    //be coded before? So how can the delete button be preset to delete something which hasn't been created yet?
+    // when a new todo is added, maybe spawn a delete button under it that is specific somehow to that entry?
 }
 
 function unrenderTodo(){
@@ -44,6 +51,6 @@ compile()
 
 document.todo.addEventListener("submit", function(event){
     event.preventDefault()
-    createTodo(event.target.title.value, /*add other values */ )
+    createTodo(event.target.title.value, event.target.description.value, event.target.price.value)
     .then(response => compile())
 })
