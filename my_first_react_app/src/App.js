7d2c1fbbe51ec19e9c831ce12e10a141a8251302
Jon Zaro <writetojonzaro@gmail.com>
2021-02-16 12:37:45 -0800

    first react

--- my_first_react_app/src/App.js
+++ my_first_react_app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Card from "./Card"
+import vacationSpots from "./vacationSpots"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const vacaCard = vacationSpots.map(spot => <Card place={spot.place} price={spot.price} timeToGo={spot.timeToGo} /> )
+    
+    
+    return (
+        <div>
+            {vacaCard}            
+        </div>
+    )
 }
 
-export default App;
+
+export default App
