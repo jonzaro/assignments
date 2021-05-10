d2f2edaf1385845b53cfeba0eaf3593acc6be958
Jon Zaro <writetojonzaro@gmail.com>
2021-05-10 15:19:43 -0700

    friends_with_pets

--- friends_with_pets/src/App.js
+++ friends_with_pets/src/App.js
@@ -1,12 +1,11 @@
 import React from "react"
 import Friend from "./Friend"
-import Pet from "./Pet"
 import FriendList from "./FriendList"
 
 function App() {
 
-  const newPet = FriendList.map(pet => <Pet key={pet.pets[0].id} name={pet.pets[0].name} breed={pet.pets[0].breed} />)
-  const newFriend = FriendList.map(friend => <Friend key={friend.id} name={friend.name} age={friend.age} pets={newPet} /> )
+  // const newPet = FriendList.map(pet => <Pet key={pet.pets[0].id} name={pet.pets[0].name} breed={pet.pets[0].breed} />)
+  const newFriend = FriendList.map(friend => <Friend key={friend.id} name={friend.name} age={friend.age} pets={friend.pets} /> )
   return (
     <div>
       {newFriend}
--- friends_with_pets/src/Friend.js
+++ friends_with_pets/src/Friend.js
@@ -1,11 +1,13 @@
 import React from "react"
+import Pet from "./Pet"
 
 function Friend(props) {
+    const newPet = props.pets.map(pet => <Pet key={pet.key} name={pet.name} breed={pet.breed} />)
     return (
         <div>
             <h3>Name: {props.name}</h3>
             <h3>Age: {props.age}</h3>
-            <h3>{props.pets}</h3>
+            {newPet}
         </div>
     )
 }
